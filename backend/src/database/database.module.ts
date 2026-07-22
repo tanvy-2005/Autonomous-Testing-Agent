891a0246ddc37b165b437ce99a22be37e65d53fc
tanvy-2005 <tanvy.pandey2023@ssipmt.com>
2026-07-22 13:01:10 +0530

    Refactor backend to use MongoDB and Mongoose instead of PostgreSQL and Prisma

---
+++ backend/src/database/database.module.ts
@@ -0,0 +1,18 @@
+import { Global, Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Global()
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  exports: [MongooseModule],
+})
+export class DatabaseModule {}
